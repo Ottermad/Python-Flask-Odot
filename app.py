954d316c64480eacd0c87188449a17260cc09f3c
Ottermad <charlie.thomas@attwoodthomas.net>
2014-12-31 16:19:19 +0000

    Init

---
+++ app.py
@@ -0,0 +1,112 @@
+# Import statements
+from db_functions import (
+    create_todo_list,
+    get_todo_list,
+    get_todo_lists,
+    update_todo_list,
+    delete_todo_list
+)
+
+from flask import (
+    Flask,
+    render_template,
+    flash,
+    redirect,
+    url_for,
+    request
+)
+
+# Flask App Setup
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "some_really_long_random_string_here"
+
+# Routes
+
+
+@app.route("/")
+@app.route("/show")
+def show():
+    """Main page of app
+
+    Features:
+    Listing of all todo lists
+    Links to viewing, editing and deleting todo lists
+    Link to add todo list
+    """
+    todo_lists = get_todo_lists()
+    context = {
+        "todo_lists": todo_lists
+    }
+    return render_template("show.html", **context)
+
+
+@app.route("/a_list/<id>")
+def a_list(id):
+    """Page for individual todo list
+
+    It takes an id parameter which is
+    uses to search for a todo list.
+    It displays the title and the
+    description for the todo list
+    """
+    my_todo_list = get_todo_list(id)
+    context = {
+        "title": my_todo_list["title"],
+        "description": my_todo_list["description"]
+    }
+    return render_template("a_list.html", **context)
+
+
+@app.route("/delete/<id>")
+def delete(id):
+    delete_list_result = delete_todo_list(id)
+    flash(delete_list_result)
+    return redirect(url_for("show"))
+
+
+@app.route("/edit/<id>", methods=["POST", "GET"])
+def edit(id):
+    """Function to display a form to/and edit a given todo list.
+
+    If the request method is POST it creates an a dictionary with
+    the id parameter, the title and description from the POST data.
+    Using this dictionary it creates a new entry in the db.
+
+    If the request method is GET it displays the form to edit the
+    todo list. Which POSTs the updated info back here.
+    """
+    if request.method == "POST":
+        my_todo_list = {
+            "id": id,
+            "title": request.form["title"],
+            "description": request.form["description"]
+        }
+        result = update_todo_list(my_todo_list)
+        flash(result)
+        return redirect(url_for("show"))
+    else:
+        return render_template("edit.html")
+
+
+@app.route("/add", methods=["POST", "GET"])
+def add():
+    """Function to display a form to add a todo list.
+
+    If the request method is POST it creates a todo list
+    using the POST data/
+
+    If the request method is GET it displays the form to add a
+    todo list. Which POSTs the info back here.
+    """
+    if request.method == "POST":
+        result = create_todo_list(
+            request.form["title"],
+            request.form["description"]
+        )
+        flash(result)
+        return redirect("/show")
+    else:
+        return render_template("add.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db_functions.py
@@ -0,0 +1,127 @@
+from peewee import (
+    MySQLDatabase,
+    CharField,
+    TextField,
+    Model
+)
+
+# DB Setup
+db = MySQLDatabase("ODOT_DB", user="root", passwd="OttersR0ck")
+
+# Models
+
+
+class BaseModel(Model):
+    """A model/class to act as a base for other models.
+
+    This is to keep my code DRY by setting common elements in one place
+    e.g. the db
+    """
+    class Meta:
+        database = db
+
+
+class TodoList(BaseModel):
+    """A model for Todo Lists which have a title and a description"""
+    _title = CharField()
+    _description = TextField()
+
+
+# Functions
+
+
+def create_todo_list(title, description):
+    """A function to create a new TodoList
+
+    It take 2 arguments - a title and a description -
+    and creates a new entry in the TodoList table.
+    An auto incrementing id is automatically created to
+    act as a primary key
+    """
+    try:
+        new_todolist = TodoList(_title=title, _description=description)
+        new_todolist.save()
+        return "Success"
+    except:
+        return "Failed"
+
+
+def get_todo_lists():
+    """A function to get all the todo lists from the db.
+
+    It returns the todo lists in a list of dictionaries.
+    The dictionaries contain an id, title and description.
+    """
+    todo_lists = []
+    for todo_list in TodoList.select():
+        todo_lists.append(
+            {
+                "id": todo_list.id,
+                "title": todo_list._title,
+                "description": todo_list._description
+            }
+        )
+    return todo_lists
+
+
+def get_todo_list(id):
+    """A function to get a single todo list from its id
+
+    It takes one argument: id which is used to return a
+    dictionary of the corresponding todo list. The
+    dictionary contains an id, a title, and a description
+    """
+    my_todo_list = TodoList.get(id=id)
+    my_todo_list_dict = {
+        "id": my_todo_list.id,
+        "title": my_todo_list._title,
+        "description": my_todo_list._description
+    }
+    return my_todo_list_dict
+
+
+def update_todo_list(todo_list):
+    """A function to update a given todo list.
+
+    It takes one argument which is a dictionary
+    containing an id, a title and a description.
+
+    It then finds the corresponding todo list based
+    on the id the updates the title and description
+    with the values from the dictionary.
+
+    It returns Success if the updates was successful
+    and Error if not.
+    """
+    try:
+        my_todo_list = TodoList.get(TodoList.id == todo_list["id"])
+        print 1
+        my_todo_list._title = todo_list["title"]
+        print 2
+        my_todo_list._description = todo_list["description"]
+        print 3
+        my_todo_list.save()
+        print 4
+        return "Success"
+
+    except:
+        return "Error"
+
+
+def delete_todo_list(id):
+    """A function to delete a todo list
+
+    It takes one argument: an id of a todo
+    list. It finds the matching todo list
+    and deletes it.
+
+    It returns Success if the updates was successful
+    and Error if not.
+    """
+    try:
+        my_todo_list = TodoList.get(TodoList.id == id)
+        my_todo_list.delete_instance()
+        return "Success"
+
+    except:
+        return "Error"
